916fe7f9938bd748d28e368ccd1a0436769f6317
Your Name <you@example.com>
2025-06-29 19:05:43 -0400

    completed part 4

--- public/js/index.js
+++ public/js/index.js
@@ -1,57 +1,7 @@
-async function getRandomImage() {
-    const client_id = "";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
-    try {
-        const response = await fetch(endpoint);
-        const returnedData = await response.json()
-        const receivedPhotoUrl = returnedData.urls.regular;
 
-        const imgDiv = document.querySelector(".background-img");
-        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+fetch('/api/v1/getRandomImage')
+  .then(res => res.json())
+  .then(data => {
+    document.querySelector('.background-img').style.backgroundImage = `url('${data.data}')`;
+  });
 
-        console.log(returnedData)
-    } catch (error) {
-        console.error(error)
-    }
-}
-getRandomImage();
-
-
-
-// "use strict"
-
-// const elements = {
-//     quote: document.getElementById("quote"),
-//     author: document.getElementById("author"),
-// };
-
-// const quotes = [
-//     {
-//         quote: "All hands! Abandon ship!",
-//         author: "Captain Picard",
-//     },
-
-//     {
-//         quote: "Doh!",
-//         author: "Homer Simpson",
-//     },
-
-//     {
-//         quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
-//         author: "Eric Schmidt",
-//     }
-// ]
-
-//     function loopThroughQuotes() {
-//     let quoteIndex = 0;
-//     setInterval(() => {
-//         if (quoteIndex < quotes.length) {
-//             elements.quote.textContent = quotes[quoteIndex].quote;
-//             elements.author.textContent = quotes[quoteIndex].author;
-//             quoteIndex++;
-//         } else {
-//             quoteIndex = 0;
-//         }
-//     }, 3000);
-// }
-// setTimeout(loopThroughQuotes, 3000);
